98b7b552e34714c34ba2152fd275460137bd0113
huohuohahei <710823927@qq.com>
2023-07-31 10:00:55 +0800

    注释更新

--- controller/feed.go
+++ controller/feed.go
@@ -1,12 +1,13 @@
 package controller
 
 import (
-	"github.com/gin-gonic/gin"
 	"math"
 	"net/http"
 	"strconv"
 	"time"
 	"x-tiktok/service"
+
+	"github.com/gin-gonic/gin"
 )
 
 type FeedResponse struct {
@@ -16,6 +17,14 @@ type FeedResponse struct {
 }
 
 // Feed 不限制登录状态，返回按投稿时间倒序的视频列表，视频数由服务端控制，单次最多30个
+// 接口详情：
+//
+//	参数：latest_time	最新投稿时间戳，不填则表示当前时间
+//		  token			用户登录后才带上
+//	返回值：
+//			status_code	0成功，其他失败
+//			status_msg	返回状态描述
+//			next_time	本次返回视频中发布时间最早的时间，作为下次请求的latest_time
 func Feed(c *gin.Context) {
 	latestTime := c.Query("latest_time")
 	//log.Println("返回视频的最新投稿时间戳:", latestTime)
